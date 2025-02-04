f1dc9fd4947ef13571e9245732ef4ca429f5950d
jeonghyeon-ohk <ok6737@naver.com>
2025-02-04 12:12:45 +0900

    admin modify

--- src/main/java/com/example/auth/global/Rq.java
+++ src/main/java/com/example/auth/global/Rq.java
@@ -19,12 +19,15 @@ public class Rq {
     private final MemberService memberService;
 
     public Member getAuthenticatedActor() {
+
         String authorizationValue = request.getHeader("Authorization");
         String apiKey = authorizationValue.substring("Bearer ".length());
         Optional<Member> opActor = memberService.findByApiKey(apiKey);
+
         if (opActor.isEmpty()) {
             throw new ServiceException("401-1", "잘못된 비밀번호 입니다.");
         }
+
         return opActor.get();
     }
 }
